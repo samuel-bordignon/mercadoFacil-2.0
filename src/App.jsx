ae01716396ae08621a4fe0782ce2810858567b7a
MAYARA STORL DE CAMARGO <mayara_s_camargo@estudante.sesisenai.org.br>
2024-10-23 17:25:19 -0300

    Comecei a tela do usuario cliente

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import UserPerfil from './pages/UserPerfil'
 
 function App() {
 
   return (
     <>
+
 
     </>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -149,7 +149,7 @@ function Navbar() {
         color: '#fff',
       }
     })
-  
+  }
 
   const enviaLista = () => {}
 
