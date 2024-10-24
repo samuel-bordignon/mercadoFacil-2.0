6ada09a59e1d821d372fbcc30818fd57ab3481ff
samuelBordignon <samuelborwie@gmail.com>
2024-10-24 11:21:50 -0300

    alterações na navbar 24/10

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import { NavLink, useLocation, useNavigate } from "react-router-dom"
 import { useEffect, useState } from 'react';
-import './Navbar.css'
 import SearchBar from "./SearchBar"
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import './Navbar.css'
+import './PopUpListaCompras.css'
+import './PopUpEnderecos.css'
 
 function Navbar() {
   const location = useLocation()
@@ -11,13 +13,12 @@ function Navbar() {
 
   // Estado único para controlar qual pop-up está aberto
   const [activePopup, setActivePopup] = useState(null)
-  const [activeAdderes, setActiveAdderes] = useState(null)
+  const [enderecoAtivo, setEnderecoAtivo] = useState(null)
+  const [prontaEnviar, setProntaEnviar] = useState(false)
   const [enderecosdb, setEnderecosdb] = useState([
     { id: 1, endereco: "Endereço 1", numero: "84", cep: "88058089", atual: true },
     { id: 2, endereco: "Endereço 2", numero: "72", cep: "88058080", atual: false },
     { id: 3, endereco: "Endereço 3", numero: "91", cep: "88058087", atual: false },
-
-
   ])
   const [produtosdb, setProdutosdb] = useState([
     {
@@ -54,7 +55,7 @@ function Navbar() {
       quantidade: 1,
       imagem: "oleo.png",
       descricao: "Óleo de soja 100% natural.",
-      informacaoAdicional: { peso: null, unidade: "1" }
+      informacaoAdicional: { peso: "1L", unidade: "1" }
     },
     {
       id: 5,
@@ -66,15 +67,20 @@ function Navbar() {
       informacaoAdicional: { peso: "1kg", unidade: "1" }
     }
   ])
-  const [listaComprasNavdb, setListaComprasNavdb] = useState(
-    {id: 1, nome: "Lista 1", produtos: null },
+  const [listaComprasNavdb, setListaComprasNavdb] = useState({
+    id: 1, nome: "Lista 1", produtos: produtosdb },
   )
   const [mercadosdb, setMercadosdb] = useState([
-    { id: 1, nome: "Mercado do João", endereco: "Rua das Flores, 123", cep: "88058089", logo: "1.png", atual: 'oi' },
-    { id: 2, nome: "Mercado do José", endereco: "Rua das Palmeiras, 456", cep: "88058080", logo: "2.png", atual: false },
-    { id: 3, nome: "Mercado do Pedro", endereco: "Rua das Oliveiras, 789", cep: "88058087", logo: "3.png", atual: false },
+    { id: 1, nome: "Mercado do João", endereco: "Rua das Flores, 123", cep: "88058089", logo: "1.png", atual: false, celular: '554899749819' },
+    { id: 2, nome: "Mercado do José", endereco: "Rua das Palmeiras, 456", cep: "88058080", logo: "2.png", atual: true, celular: '554899749819' },
+    { id: 3, nome: "Mercado do Pedro", endereco: "Rua das Oliveiras, 789", cep: "88058087", logo: "3.png", atual: false, celular: '554899749819' },
   ])
-
+  const[usuario, setUsuario] = useState({
+    id: 1,
+    nome: "Palhaço Caçarola",
+    email: "caçarola@palhaço.com",
+  })
+  
   // Função para exibir mensagens de erro/validação
   const showErrorToast = () => {
     toast.error("Erro ao carregar dados do usuário!")
@@ -93,10 +99,10 @@ function Navbar() {
   }
 
   const toggleAdderess = (cep) => {
-    if (activeAdderes === cep) {
+    if (enderecoAtivo === cep) {
       return
     } else {
-      setActiveAdderes(cep)
+      setEnderecoAtivo(cep)
       setAdderessAtual(cep)
     }
   }
@@ -149,9 +155,28 @@ function Navbar() {
         color: '#fff',
       }
     })
-  
+  }
 
-  const enviaLista = () => {}
+  const atualizarListaCompras = () => {
+    setListaComprasNavdb({ ...listaComprasNavdb, produtos: produtosdb });
+    setProntaEnviar(true)
+  }
+
+  function concatenaProdutos() {
+    return (listaComprasNavdb.produtos.map((item) => `${item.nome} - ${item.quantidade} un`).join('\n'))
+  }
+
+  useEffect(() => {
+    if (prontaEnviar) {
+      const mensagem = `Olá! Gostaria de fazer um pedido com os seguintes itens:\n\n${concatenaProdutos()}\n\n*Endereço de entrega:* ${enderecosdb.find(e => e.atual === true)?.endereco}, ${enderecosdb.find(e => e.atual === true)?.numero}\n\n*Total* ${calcularTotal()}\n\nAtenciosamente, ${usuario.nome}`
+      const numero = mercadosdb.find(m => m.atual === true).celular
+      const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`
+      window.open(url, '_blank')
+      setProntaEnviar(false)
+    }
+
+  }, [produtosdb, listaComprasNavdb, prontaEnviar, enderecosdb])
+
 
   return (
     <div className="navbar">
@@ -185,11 +210,11 @@ function Navbar() {
       {/* Container Direito */}
       <div id="container-direito-nav">
         {/* Botão Endereço - Pop-up */}
-        <button id="address-button" onClick={() => togglePopup('address')}>
-          <div className="address-container">
-            <span className="address-text">
+        <button id="endereco-button" onClick={() => togglePopup('endereco')}>
+          <div className="endereco-container">
+            <span className="endereco-text">
               {enderecosdb.find(e => e.atual === true)?.endereco ? `${enderecosdb.find(e => e.atual === true).endereco}, ` : "Selecione um endereço"}
-              <span className="address-number">
+              <span className="endereco-number">
                 {enderecosdb.find(e => e.atual === true)?.numero || ""}
               </span>
             </span>
@@ -198,9 +223,9 @@ function Navbar() {
           </div>
         </button>
 
-        {activePopup === 'address' && (
+        {activePopup === 'endereco' && (
           <div onClick={() => { setActivePopup(null) }} className="overlay">
-            <div className="popup-address" onClick={(e) => e.stopPropagation()}>
+            <div id="popup-endereco" onClick={(e) => e.stopPropagation()}>
               <div className="btn-popup-container">
                 <button
                   className="btn-popup"
@@ -209,7 +234,7 @@ function Navbar() {
                   <img src="Xverde.svg" alt="X" />
                 </button>
               </div>
-              <button className="add-adderress">
+              <button className="add-endereco">
                 <div>
                   <img src="adicionarIcon.svg" alt="Adicionar" />
                 </div>
@@ -218,7 +243,7 @@ function Navbar() {
               {enderecosdb.map((endereco) => (
                 <button
                   key={endereco.id}
-                  className={activeAdderes === endereco.cep || endereco.atual ? 'adderess-um-atual' : 'adderess'}
+                  className={enderecoAtivo === endereco.cep || endereco.atual ? 'adderess-atual' : 'adderess'}
                   onClick={() => toggleAdderess(endereco.cep)}
                 >
                   <span className="cep-text-pop">{endereco.cep}</span>
@@ -244,8 +269,8 @@ function Navbar() {
             </div>
           </button>
           {activePopup === 'list' && (
-            <div onClick={() => { setActivePopup(null) }} className="overlay">
-              <div className="popup-list" onClick={(e) => e.stopPropagation()}>
+            <div onClick={() => { setActivePopup(null) }} className="overlay-list">
+              <div id="popup-list" onClick={(e) => e.stopPropagation()}>
                 <button
                   className="btn-popup"
                   onClick={() => { setActivePopup(null) }}
@@ -254,13 +279,13 @@ function Navbar() {
                 </button>
                 <div className="shopping-list">
                   {mercadosdb.map((mercado) => {
-                    if (mercado.atual === 'oi') {
+                    if (mercado.atual) {
                       return <div key={mercado.id} className="market-list">
                         <div className="logo-name">
                           <img src={mercado.logo} alt="Logo do mercado" className="logo-mercado" />
                           <span className="market-name">{mercado.nome}</span>
                         </div>
-                        <NavLink to="/mercado" className="change-market">Ver Catálogo</NavLink>
+                        <NavLink to="/mercado" className="visitar-mercado">Ver Catálogo</NavLink>
                       </div>
                     }
                   })}
@@ -315,7 +340,7 @@ function Navbar() {
                     <button className="comparar-btn" onClick={() => navigate('/comparacaoListas')}>
                       Comparar Preços
                     </button>
-                    <button className="enviar-btn" onClick={customImageToast}>
+                    <button className="enviar-btn" onClick={() => atualizarListaCompras()}>
                       Enviar Lista
                     </button>
                   </div>
