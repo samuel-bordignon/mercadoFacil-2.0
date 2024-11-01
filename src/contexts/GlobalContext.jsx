ea5f9b286cddc75b58225297c50f9e6612f2bde7
samuelBordignon <samuelborwie@gmail.com>
2024-11-01 17:05:30 -0300

    rumando uns errinhos bobos

--- src/contexts/GlobalContext.jsx
+++ src/contexts/GlobalContext.jsx
@@ -6,15 +6,7 @@ export const GlobalContext = createContext()
 // Provedor do contexto que irá envolver o app
 export const GlobalContextProvider = ({ children }) => {
     // Estado para endereços
-<<<<<<< HEAD
     const [enderecosdb, setEnderecosdb] = useState([])
-=======
-    const [enderecosdb, setEnderecosdb] = useState([
-        // { id: 1, complemento: "Apto 10", numero: "84", cep: "88010-001", logradouro: "Rua Sao João", bairro: "Rio Vermelho", apelido: "Minha casa", atual: true },
-        // { id: 2, complemento: "Casa 23", numero: "72", cep: "88058-080", logradouro: "Rua Sao João", bairro: "Rio Vermelho", apelido: "Minha casa", atual: false },
-        // { id: 3, complemento: "Condominio", numero: "91", cep: "88058-087", logradouro: "Rua Sao João", bairro: "Rio Vermelho", apelido: "Minha casa", atual: false },
-    ])
->>>>>>> 5a812fdf049027fe7fe3357c3811851a35079b83
     // Estado para produtos
     const [produtosdb, setProdutosdb] = useState([
         { id: 1, nome: "Arroz Branco", preco: 10.00, quantidade: 1, imagem: "arroz.png", informacaoAdicional: { peso: "5", unidade: "kg" } },
@@ -25,9 +17,6 @@ export const GlobalContextProvider = ({ children }) => {
         { id: 6, nome: "Farinha de Trigo", preco: 4.20, quantidade: 1, imagem: "farinha.png", informacaoAdicional: { peso: "1", unidade: "kg" } },
     ])
     // Estado para mercados
-<<<<<<< HEAD
-    const [mercadosdb, setMercadosdb] = useState([{}])
-=======
     const [mercadosdb, setMercadosdb] = useState([
         { id: 1, nome: "Mercado do João", endereco: "Rua das Flores, 123", cep: "88058089", logo: "1.png", atual: true, celular: 554899749819 },
         { id: 2, nome: "Mercado do José", endereco: "Rua das Palmeiras, 456", cep: "88058080", logo: "2.png", atual: false, celular: 554899749819 },
@@ -43,7 +32,6 @@ export const GlobalContextProvider = ({ children }) => {
       ];
 
 
->>>>>>> 5a812fdf049027fe7fe3357c3811851a35079b83
     // Estado para Usuário logado
     const [clientedb, setClientedb] = useState({})
     // Estado para Parceiro logado
@@ -71,33 +59,16 @@ export const GlobalContextProvider = ({ children }) => {
 
 
 
-<<<<<<< HEAD
-    // Prover estados e funções aos componentes filhos
-    return (
-        <GlobalContext.Provider value={{
-            enderecosdb, setEnderecosdb,
-            produtosdb, setProdutosdb,
-            mercadosdb, setMercadosdb,
-            clientedb, setClientedb,
-            parceirodb, setParceirodb
-        }}>
-            {children}
-        </GlobalContext.Provider>
-    )
-}
-=======
 // Prover estados e funções aos componentes filhos
 return (
     <GlobalContext.Provider value={{
         enderecosdb, setEnderecosdb,
         produtosdb, setProdutosdb,
         mercadosdb, setMercadosdb,
-        usuariodb, setUsuariodb,
-        listaEnderecos, setListaEnderecos,
+        clientedb, setClientedb,
         mercadosVisitados // Adicionado aqui
     }}>
         {children}
     </GlobalContext.Provider>
 )
 }
->>>>>>> 5a812fdf049027fe7fe3357c3811851a35079b83
