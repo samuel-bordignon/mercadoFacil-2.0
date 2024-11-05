a414b355b2f4b3b5b3cc284630f81285bab207c6
dudawl18 <mwolfluiz123a3@gmail.com>
2024-11-05 00:53:46 -0300

    Trabalhando na sideBar da telaDentroMercado

--- src/pages/TelaDentroMercado.jsx
+++ src/pages/TelaDentroMercado.jsx
@@ -6,11 +6,18 @@ function TelaDentroMercado() {
   return (
     <div className="tudo">
       <Navbar />
-      <div className="tela-dentro-mercado"> 
+      <div className="tela-dentro-mercado">
         <div className="sideBar-dentro-mercado">
-          <input type="text" />
+          <div className="nome-mercado-container">
+            <h5>Mercado Nome</h5> {/* Exemplo, será alterado (mercado.nome)*/}
+          </div>
+          <div className="endereco-cnpj-container">
+            <p className="sub-titulo-sideBar-mercado">Sobre</p>
+            <h5>Endereço</h5>
+            <p>informações sobre o endereço do mercado</p>
+            <p>CEP</p> {/*digitar o cep */}
+          </div>
         </div>
-        <div></div>
       </div>
 
     </div>
