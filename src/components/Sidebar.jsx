a4e066952182bd2a7bc42a8b6cb1d5a65a455989
GABRIEL FERNANDES LACERDA <gabriel_f_lacerda@estudante.sesisenai.org.br>
2024-11-05 14:30:15 -0300

    SideBar pronta

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,25 +1,41 @@
 import './Sidebar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 const Sidebar = () => {
+    const [activeItem, setActiveItem] = useState(null);
+
+    const handleItemClick = (item) => {
+        setActiveItem(item);
+    };
+
     return (
         <div>
             <nav id="sidebar">
                 <div id="sidebar_content">
                     <ul id="side_items">
-                        <div className='icon-Top'>
+                        <div className="icon-Top">
                             <img className="icon-mercado" src="iconsidebar.svg" alt="" />
                         </div>
-                        <li className="side-item">
+                        <li
+                            className={`side-item ${activeItem === 'Estoque' ? 'active' : ''}`}
+                            onClick={() => handleItemClick('Estoque')}
+                        >
                             <i className="bi bi-box-seam fa-2x"></i>
                             <span>Estoque</span>
                         </li>
-                        <li className="side-item">
+                        <li
+                            className={`side-item ${activeItem === 'Mercado' ? 'active' : ''}`}
+                            onClick={() => handleItemClick('Mercado')}
+                        >
                             <i className="bi bi-shop fa-2x"></i>
                             <span>Mercado</span>
                         </li>
-                        <li className="side-item">
+                        <li
+                            className={`side-item ${activeItem === 'Gerente' ? 'active' : ''}`}
+                            onClick={() => handleItemClick('Gerente')}
+                        >
                             <i className="bi bi-person-circle fa-2x"></i>
                             <span>Gerente</span>
                         </li>
