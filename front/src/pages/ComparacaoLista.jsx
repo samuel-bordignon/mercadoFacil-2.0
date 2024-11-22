d05ac838f5b81c72139ee95edb3c426f0e624846
Mayara Storl <mayarastorl@gmail.com>
2024-11-22 17:25:25 -0300

    telas de comparação de lista 85% feitas

--- front/src/pages/ComparacaoLista.jsx
+++ front/src/pages/ComparacaoLista.jsx
@@ -17,31 +17,87 @@ function ComparacaoLista() {
 
         <div className='info-localizacao'>
         <img src="/public/endereco.png" alt="" />
+        <div className='text-loc'>
         <h3>Endereço do mercado</h3>
         <h4>Complemento</h4>
         </div>
+        </div>
 
+        <div className='horario-atendimento'>
+        <h3>20 - 30 min</h3>
+        </div>
+
+        <div className='container-card-mer'>
         <div className='card-mer'>
         <h4>Nome do Mercado</h4>
         <p>2,4 Km</p>
-        <h4>R$ 15,99</h4>
+        <h5>R$ 15,99</h5>
 
+        </div>
+
+        <div className='card-mer-2'>
+        <h4>Nome do Mercado</h4>
+        <p>2,4 Km</p>
+        <h5>R$ 15,99</h5>
+
+        </div>
+
+        <div className='card-mer-2'>
         <h4>Nome do Mercado</h4>
         <p>2,4 Km</p>
         <h4>R$ 15,99</h4>
 
-        <h4>Nome do Mercado</h4>
-        <p>2,4 Km</p>
-        <h4>R$ 15,99</h4>
+        </div>
 
+        <div className='card-mer-2'>
         <h4>Nome do Mercado</h4>
         <p>2,4 Km</p>
         <h4>R$ 15,99</h4>
         </div>
 
+        </div>
+
+        <div className='bot-enviar-lista'>
         <button>Enviar Lista</button>
 
-      </div>
+        </div>
+        </div>
+
+        <div className='lista-merc'>
+
+          <div className='cabecario-list'>
+          <div className='lugar-da-foto-redonda'></div>
+          <p>Nome do mercado</p>
+          <button>Ver catálago</button>
+          </div>
+
+          <hr />
+
+          <div className='produto-nao-encontrado'>
+          <h2>Não encontramos 2 itens da lista</h2>
+          <div className='lugar-da-foto-quadrada'></div>
+          <h3>Nome do mercado</h3>
+          <h5>Peso/Qualquer coisa</h5>
+          <h3>R$ 0,00</h3>
+          <p>Remover</p>
+          </div>
+          
+          <div className='add'>
+          <i class="bi bi-dash"></i>
+          <h5>1</h5>
+          <i class="bi bi-plus"></i>
+          </div>
+          <hr />
+          <h3>Nome do mercado</h3>
+          <h5>Peso/Qualquer coisa</h5>
+          <h3>R$ 0,00</h3>
+          <p>Remover</p>
+          <i class="bi bi-dash"></i>
+          <h3>1</h3>
+          <i class="bi bi-plus"></i>
+
+        </div>
+
 
 
     </div>
