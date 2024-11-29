69a7ece613bf3b0b82370f33069ede280e090cd5
Mayara Storl <mayarastorl@gmail.com>
2024-11-29 17:13:54 -0300

    Tela de comparação de lista possivelmente pronta?

--- front/src/pages/ComparacaoLista.jsx
+++ front/src/pages/ComparacaoLista.jsx
@@ -4,7 +4,7 @@ import './ComparacaoLista.css'
 function ComparacaoLista() {
   return (
     <div>
-      <div className='comparacaoLista'>
+      <div className='navBar-comparaLista'>
         <button>Voltar</button>
         <img src="/public/logo.png" alt="" />
       </div>
@@ -29,29 +29,41 @@ function ComparacaoLista() {
 
         <div className='container-card-mer'>
         <div className='card-mer'>
-        <h4>Nome do Mercado</h4>
-        <p>2,4 Km</p>
-        <h5>R$ 15,99</h5>
-
+        <div className='cabecario-mer'>
+        <div className='logo-mercado-compara-lista'></div>
+        <h3>Nome do Mercado</h3>
         </div>
-
-        <div className='card-mer-2'>
-        <h4>Nome do Mercado</h4>
-        <p>2,4 Km</p>
-        <h5>R$ 15,99</h5>
-
-        </div>
-
-        <div className='card-mer-2'>
-        <h4>Nome do Mercado</h4>
-        <p>2,4 Km</p>
+        <h4>2,4 Km</h4>
         <h4>R$ 15,99</h4>
 
         </div>
 
         <div className='card-mer-2'>
-        <h4>Nome do Mercado</h4>
-        <p>2,4 Km</p>
+        <div className='cabecario-mer'>
+        <div className='logo-mercado-compara-lista' ></div>
+        <h3>Nome do Mercado</h3>
+        </div>
+        <h4>2,4 Km</h4>
+        <h4>R$ 15,99</h4>
+
+        </div>
+
+        <div className='card-mer-2'>
+        <div className='cabecario-mer'>
+        <div className='logo-mercado-compara-lista'></div>
+        <h3>Nome do Mercado</h3>
+        </div>
+        <h4>2,4 Km</h4>
+        <h4>R$ 15,99</h4>
+
+        </div>
+
+        <div className='card-mer-2'>
+        <div className='cabecario-mer'>
+        <div className='logo-mercado-compara-lista'></div>
+        <h3>Nome do Mercado</h3>
+        </div>
+        <h4>2,4 Km</h4>
         <h4>R$ 15,99</h4>
         </div>
 
@@ -63,10 +75,12 @@ function ComparacaoLista() {
         </div>
         </div>
 
+{/* SidBar da lista de produtos não encontrados e que precisaram ser substituidos */}
+
         <div className='lista-merc'>
 
           <div className='cabecario-list'>
-          <div className='lugar-da-foto-redonda'></div>
+          <div className='foto-redonda'></div>
           <p>Nome do mercado</p>
           <button>Ver catálago</button>
           </div>
@@ -74,11 +88,11 @@ function ComparacaoLista() {
           <hr />
 
           <div className='produto-nao-encontrado'>
-          <h2>Não encontramos 2 itens da lista</h2>
-          <div className='foto-quadrada'>
+          <h4>Não encontramos 2 itens da lista</h4>
+          <div className='foto-produto'>
           <img src="oleo.png" alt="" />
           <div className='detalhe'>
-          <h3>Nome do mercado</h3>
+          <h3>Nome do produto</h3>
           <h5>Peso/Qualquer coisa</h5>
           <h3>R$ 0,00</h3>
           </div>
@@ -96,19 +110,64 @@ function ComparacaoLista() {
           
           <hr />
 
+          <div className='produto-nao-encontrado'>
+          <div className='foto-produto'>
+          <img src="oleo.png" alt="" />
           <div className='detalhe'>
           <h3>Nome do mercado</h3>
           <h5>Peso/Qualquer coisa</h5>
           <h3>R$ 0,00</h3>
           </div>
-          <div className='add-icon'>
-          <p className='remover'>Remover</p>
-          <i class="bi bi-dash"></i>
-          <h3>1</h3>
-          <i class="bi bi-plus"></i>
-
+          </div>
+          <div className='add'>
+          <button className='remover'>Remover</button>
+          <i class="bi bi-dash-lg"></i>
+          <h5>1</h5>
+          <i class="bi bi-plus-lg"></i>
+          </div>
           </div>
 
+          <hr />
+
+          <div className='produto-nao-encontrado'>
+          <h4>Substituimos pelos seguintes itens</h4>
+          <div className='foto-produto'>
+          <img src="oleo.png" alt="" />
+          <div className='detalhe'>
+          <h3>Nome do produto</h3>
+          <h5>Peso/Qualquer coisa</h5>
+          <h3>R$ 0,00</h3>
+          </div>
+          </div>
+          </div>
+
+          <div className='add'>
+          <button className='but-remover'>Remover</button>
+          <div className='menosÉmais'>
+          <i class="bi bi-dash"></i>
+          <h5>1</h5>
+          <i class="bi bi-plus"></i>
+          </div>
+          </div>
+          
+          <hr />
+
+          <div className='produto-nao-encontrado'>
+          <div className='foto-produto'>
+          <img src="oleo.png" alt="" />
+          <div className='detalhe'>
+          <h3>Nome do produto</h3>
+          <h5>Peso/Qualquer coisa</h5>
+          <h3>R$ 0,00</h3>
+          </div>
+          </div>
+          <div className='add'>
+          <button className='remover'>Remover</button>
+          <i class="bi bi-dash-lg"></i>
+          <h5>1</h5>
+          <i class="bi bi-plus-lg"></i>
+          </div>
+          </div>
         </div>
 
 
